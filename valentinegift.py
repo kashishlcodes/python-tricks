0117043c5d01ce66861740582b5aa0f7a5b08258
kashishlcodes <99608014+kashishlcodes@users.noreply.github.com>
2022-02-13 22:28:23 +0530

    Formatting

--- valentinegift.py
+++ valentinegift.py
@@ -1,11 +1,12 @@
 from turtle import *
-color("red","pink")
+
+color("red", "pink")
 begin_fill()
 left(50)
 forward(100)
-circle(40,180)
+circle(40, 180)
 left(260)
-circle(40,180)
+circle(40, 180)
 forward(100)
 end_fill()
 done()
